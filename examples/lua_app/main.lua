84b4bb433f1029b9feade0a8f3dee2257bda9417
CodeHorizon0 <274509296+CodeHorizon0@users.noreply.github.com>
2026-07-17 12:36:12 +0300

    Delete examples directory

--- examples/lua_app/main.lua
+++
@@ -1,5 +0,0 @@
-local host = host
-local tools = require("tools")
-
-print(tools.greet("lua"))
-print(host.cwd())
--- examples/python_app/main.py
+++
@@ -1,5 +0,0 @@
-import host
-from helpers import greet
-
-print(greet("python"))
-print(host.cwd())
