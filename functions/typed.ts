2728f2c9788fb32b221901d12dc485c088820102
CodeHorizon0 <274509296+CodeHorizon0@users.noreply.github.com>
2026-07-19 11:25:14 +0300

    Delete functions directory

--- functions/typed.ts
+++
@@ -1,7 +0,0 @@
-globalThis.handler = function(request: string): string {
-    return JSON.stringify({
-        status: 200,
-        body: "hello from typescript",
-        request: request
-    });
-};
